23592f279051691627e0c0c972f497464698bed5
Artur <coungard@mail.ru>
2020-06-11 18:38:47 +0300

    SpringBootApplication way

---
+++ src/main/java/com/coungard/EgarTestApplication.java
@@ -0,0 +1,12 @@
+package com.coungard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgarTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgarTestApplication.class, args);
+    }
+}
